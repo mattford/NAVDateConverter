b0ec0226571fd0bd4a5cca5156692e5ae88585cb
Matt Ford <mat84@btinternet.com>
2014-12-10 13:22:35 +0000

    Switched to using JodaTime for Date operations as Java Date is a nightmare. Fixed inaccurate calculated value due to month being zero-indexed, tweaked visual display, app now displays today's date on startup

--- app/src/main/java/uk/org/mattford/logicaldateconverter/MainActivity.java
+++ app/src/main/java/uk/org/mattford/logicaldateconverter/MainActivity.java
@@ -5,23 +5,16 @@ import android.app.Dialog;
 import android.app.DialogFragment;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.TextView;
 
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.concurrent.TimeUnit;
+import org.joda.time.DateTime;
+import org.joda.time.Days;
 
+import java.util.Calendar;
 
 public class MainActivity extends ActionBarActivity {
-
-    private final int MILLISECONDS_IN_A_DAY = 86400000;
-    private final String logTag = "NAVDateConverter/MainActivity";
 
     public static class DatePickerFragment extends DialogFragment
             implements DatePickerDialog.OnDateSetListener {
@@ -43,8 +36,7 @@ public class MainActivity extends ActionBarActivity {
         }
 
         public void onDateSet(DatePicker view, int year, int month, int day) {
-            // Do something with the date chosen by the user
-            parentActivity.onDateSelected(Integer.toString(year)+"-"+Integer.toString(month+1)+"-"+Integer.toString(day));
+            parentActivity.onDateSelected(year, month+1, day);
             dismiss();
         }
     }
@@ -53,60 +45,33 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+        onDateSelected(year, month+1, day);
     }
-
-    /*
-    No menu for this app.
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-    */
 
     public void onSelectDateClick(View v) {
-        DialogFragment datepicker = new DatePickerFragment();
-        datepicker.show(getFragmentManager(), "datePicker");
-
+        new DatePickerFragment().show(getFragmentManager(), "datePicker");
     }
 
-    public void onDateSelected(String selectedDate) {
-        Log.v(logTag, selectedDate);
-        Date dateToConvert = null;
-        Date yearOneDate = null;
-        DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
-        try {
-            dateToConvert = format.parse(selectedDate);
-            yearOneDate = format.parse("0001-01-03");
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        long diffMSeconds = dateToConvert.getTime() - yearOneDate.getTime();
-        long diffDays = TimeUnit.DAYS.convert(diffMSeconds, TimeUnit.MILLISECONDS);
-        Log.v(logTag, Long.toString(diffDays));
+    public void onDateSelected(int year, int month, int day) {
+        DateTime dateToConvert = null;
+        DateTime yearOneDate = null;
+        dateToConvert = new DateTime(year, month, day, 0, 0, 0);
+        yearOneDate = new DateTime(0001,01,03, 0, 0, 0);
+
+        int diffDays = Days.daysBetween(yearOneDate, dateToConvert).getDays();
+
         diffDays = (diffDays * 2) + 737;
-        Log.v(logTag, Long.toString(diffDays));
-        //diffDays = Long.reverseBytes(diffDays);
-        String converted = "0x"+Long.toHexString(diffDays);
-        //converted = converted.replaceAll("0*$", "");
+
+        diffDays = Integer.reverseBytes(diffDays);
+        String converted = Integer.toHexString(diffDays);
+        converted = converted.replaceAll("0*$", "");
+        converted = "0x"+converted.toUpperCase()+"00";
         TextView tv = (TextView)findViewById(R.id.convertedValue);
-        tv.setText(getString(R.string.output_value_text,converted));
+        tv.setText(converted);
 
     }
 }
